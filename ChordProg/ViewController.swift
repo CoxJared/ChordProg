c3b1501dfa7b307aa5c5ad4ed2d106f8e8c8099e
Jared Cox <coxj1@mcmaster.ca>
2021-01-04 21:17:13 -0500

    Generate random key and chord progression

--- ChordProg/ViewController.swift
+++ ChordProg/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var keyLbl: UILabel!
+    @IBOutlet weak var chordProgressionLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let keys = ["A♭", "A","B♭", "B", "C", "D♭", "D", "E♭", "E", "F", "G♭", "G"];
+    let progressions = ["I  IV  V  I","I  ii  V  I", "I  vi  IV  V", "I  IV  ii  V", "I  IV  vi  V",  "I  V  vi  IV"];
 
-
+    @IBAction func generateNewProgression(_ sender: Any) {
+        keyLbl.text = keys.randomElement()
+        chordProgressionLbl.text = progressions.randomElement();
+    }
+    
 }
 
