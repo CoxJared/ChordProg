df88e9c1d34c27e6a6b8f91478f6f799b24c4059
Jared Cox <coxj1@mcmaster.ca>
2021-01-05 23:14:27 -0500

    Added randomizing maj/min

--- ChordProg/ViewController.swift
+++ ChordProg/ViewController.swift
@@ -16,12 +16,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    let keys = ["A♭", "A","B♭", "B", "C", "D♭", "D", "E♭", "E", "F", "G♭", "G"];
-    let progressions = ["I  IV  V  I","I  ii  V  I", "I  vi  IV  V", "I  IV  ii  V", "I  IV  vi  V",  "I  V  vi  IV"];
+    let modes = ["major", "minor"]
+    
+    let majorKeys = ["A♭", "A","B♭", "B", "C", "D♭", "D", "E♭", "E", "F", "G♭", "G"];
+    
+    let majorProgressions = ["I  IV  V  I","I  ii  V  I", "I  vi  IV  V", "I  IV  ii  V", "I  IV  vi  V",  "I  V  vi  IV"];
+    
+    let minorKeys = ["a♭", "a", "b♭", "b", "c", "d♭", "d", "e♭", "e", "f", "g♭", "g"]
+    
+    let minorProgressions = ["i  v  IV  i", "i  IV  v  i", "i  iv  v", "ii  v  i", "i  iv  i", "VI  VII  i  i"]
+    
 
     @IBAction func generateNewProgression(_ sender: Any) {
-        keyLbl.text = keys.randomElement()
-        chordProgressionLbl.text = progressions.randomElement();
+        let mode = modes.randomElement()
+        if(mode == "major") {
+            keyLbl.text = majorKeys.randomElement()
+            chordProgressionLbl.text = majorProgressions.randomElement();
+        } else {
+            keyLbl.text = minorKeys.randomElement()
+            chordProgressionLbl.text = minorProgressions.randomElement();
+        }
     }
     
 }
